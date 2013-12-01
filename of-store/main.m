83153400aa78be2b84514e0bc2af345c55d492d3
Jan-Yves Ruzicka <jan@1klb.com>
2013-12-01 19:55:59 +1300

    Initial Commit

---
+++ of-store/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  of-store
+//
+//  Created by Jan-Yves on 1/12/13.
+//  Copyright (c) 2013 Jan-Yves Ruzicka. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
